cc1eeced30ab68976deba40f018e819b5eea90eb
PraveenMatheesha <77117024+Praveen-47@users.noreply.github.com>
2022-11-08 23:56:33 +0530

    update user dto

--- src/user/dto/update-user.dto.ts
+++ src/user/dto/update-user.dto.ts
@@ -1,4 +1,21 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateUserDto } from './create-user.dto';
+import { IsEmail,IsNotEmpty, IsString } from "class-validator";
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {}
+export class UpdateUserDto extends PartialType(CreateUserDto) {
+    @IsNotEmpty()
+    @IsString()
+    name:string;
+
+    @IsNotEmpty()
+    @IsString()
+    address:string;
+
+    @IsNotEmpty()
+    @IsString()
+    photo:string;
+
+    @IsNotEmpty()
+    @IsString()
+    accounts:string
+}
