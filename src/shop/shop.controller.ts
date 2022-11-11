24e16901c8c277eba117767fcdf684c878d4f40f
PraveenMatheesha <77117024+Praveen-47@users.noreply.github.com>
2022-11-12 01:53:21 +0530

    @UserDec to pass id to  the product

--- src/shop/shop.controller.ts
+++ src/shop/shop.controller.ts
@@ -1,9 +1,11 @@
 import { CreateShopDTO } from './dto/create-shop.dto';
-import { Controller, Get, Post } from '@nestjs/common';
+import { Controller, Get, Post, Req } from '@nestjs/common';
 import { Body, UseGuards } from '@nestjs/common/decorators';
 import { ShopService } from './shop.service';
 import { AuthGuard } from '@nestjs/passport';
 import { AdminGuard } from 'src/guards/admin.guard';
+import { UserDec } from 'src/utilities/user.decorator';
+import { User } from '../user/schema/user.schema';
 
 @Controller('shop')
 export class ShopController {
@@ -11,8 +13,17 @@ export class ShopController {
 
   @Post()
   @UseGuards(AuthGuard('jwt'), AdminGuard)
-  createProduct(@Body() createProductDTO: CreateShopDTO) {
-    return this.shopService.createProduct(createProductDTO);
+  createProduct(
+    @UserDec() user: User,
+    @Body() createProductDTO: CreateShopDTO,
+  ) {
+    console.log(user._id);
+    try {
+      // const {productname,productPrice,productphoto,producttype} = createProductDTO
+      // const {_id} = user
+
+      return this.shopService.createProduct(createProductDTO, user);
+    } catch (error) {}
   }
 
   @Get()
