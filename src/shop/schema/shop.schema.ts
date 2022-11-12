1cf66d2ae1c2e4b8a7cbb6fb9b44617f58c9a84c
PraveenMatheesha <77117024+Praveen-47@users.noreply.github.com>
2022-11-12 12:53:46 +0530

    faker js

--- src/shop/schema/shop.schema.ts
+++ src/shop/schema/shop.schema.ts
@@ -3,23 +3,35 @@ import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { HydratedDocument, SchemaType, SchemaTypes, Types } from 'mongoose';
 import { User, UserSchema } from 'src/user/schema/user.schema';
 import { Type } from 'class-transformer';
+import { Factory } from 'nestjs-seeder';
+import { faker } from '@faker-js/faker';
+import { randomInt } from 'crypto';
 export type ShopDocument = HydratedDocument<Shop>;
-export type ProductType = "Shampoo" | "Toy" | "Vitamin"
+export type ProductType = 'Shampoo' | 'Toy' | 'Food';
 
 @Schema({ timestamps: true })
 export class Shop {
-  @Prop({ type: SchemaTypes.ObjectId, ref: 'User', required: true })
+  @Prop({
+    type: SchemaTypes.ObjectId,
+    ref: 'User',
+    required: false,
+    default: '636eae357470ba9fc521b294',
+  })
   owner: Types.ObjectId;
 
+  @Factory((faker) => faker.lorem.words(2))
   @Prop()
   productname: string;
 
+  @Factory((faker) => randomInt(10, 1000))
   @Prop()
   productPrice: number;
 
+  @Factory((faker) => faker.image.cats(1234, 2345))
   @Prop()
   productphoto: string;
 
+  @Factory((faker) => faker.helpers.arrayElement(['Shampoo', 'Toy', 'Food']))
   @Prop()
   producttype: ProductType;
 }
