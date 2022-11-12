7680e0a505b17e10ba322d5753dbcd4c8ab57add
PraveenMatheesha <77117024+Praveen-47@users.noreply.github.com>
2022-11-12 16:32:07 +0530

    faker review db

--- src/review/schema/review.schema.ts
+++ src/review/schema/review.schema.ts
@@ -1,18 +1,22 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { HydratedDocument, Types } from 'mongoose';
+import { Factory } from 'nestjs-seeder';
+import { randomInt } from 'crypto';
 
-export type ReviewDocument = HydratedDocument<Review>
+export type ReviewDocument = HydratedDocument<Review>;
 
-@Schema({timestamps:true})
-export class Review{
- @Prop()
- owner:Types.ObjectId
+@Schema({ timestamps: true })
+export class Review {
+  @Prop({ default: '636eae2e7470ba9fc521b292' })
+  owner: Types.ObjectId;
 
- @Prop()
- review:string;
+  @Factory((faker) => faker.lorem.sentences(2))
+  @Prop()
+  review: string;
 
- @Prop()
- star:number;
+  @Factory((faker) => randomInt(1, 6))
+  @Prop()
+  star: number;
 }
 
-export const ReviewSchema = SchemaFactory.createForClass(Review)
+export const ReviewSchema = SchemaFactory.createForClass(Review);
