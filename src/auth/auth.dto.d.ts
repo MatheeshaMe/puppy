6b7cf4829d483616e55425b7358f454ad20ed418
matheeshame <hpraveenmatheesha@gmail.com>
2022-11-18 17:08:01 +0530

    exports AuthService

--- src/auth/auth.dto.d.ts
+++ src/auth/auth.dto.d.ts
@@ -1,13 +1,13 @@
-export interface LoginDTO{
-    name:string;
-    password:string
+export interface LoginDTO {
+  name: string;
+  password: string;
 }
 
-export interface RegisterDTO{
-    name:string;
-    address:string;
-    photo:string;
-    accounts:string;
-    password:string;
-    isAdmin:boolean
+export interface RegisterDTO {
+  name: string;
+  address: string;
+  photo: string;
+  accounts: string;
+  password: string;
+  isAdmin: boolean;
 }
