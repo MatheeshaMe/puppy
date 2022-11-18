f5172162229c82cfc1cf45b49260eb5ce02bd2bf
matheeshame <hpraveenmatheesha@gmail.com>
2022-11-18 17:11:36 +0530

    IsString? name

--- src/puppy/dto/update-puppy.dto.ts
+++ src/puppy/dto/update-puppy.dto.ts
@@ -1,43 +1,44 @@
 import {
-    ArrayMinSize,
-    IsArray,
-    IsNotEmpty,
-    IsNumber,
-    IsOptional,
-    IsString,
-  } from 'class-validator';
+  ArrayMinSize,
+  IsArray,
+  IsNotEmpty,
+  IsNumber,
+  IsOptional,
+  IsString,
+} from 'class-validator';
 
-export class UpdatePuppyDto{@IsString()
-    @IsNotEmpty()
-    name: string;
-  
-    @IsNumber()
-    @IsNotEmpty()
-    age: number;
-  
-    @IsArray()
-    @IsNotEmpty()
-    @ArrayMinSize(1)
-    photo: string[];
-  
-    @IsArray()
-    @IsNotEmpty()
-    type: string | PetType;
-  
-    @IsString()
-    @IsOptional()
-    breed?: string;
-  
-    @IsString()
-    @IsNotEmpty()
-    location: string;
-  
-    @IsString()
-    @IsNotEmpty()
-    phoneNumber: string;
-  }
-  
-  export enum PetType {
-    Dog,
-    Cat,
-  }
+export class UpdatePuppyDto {
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @IsNumber()
+  @IsNotEmpty()
+  age: number;
+
+  @IsArray()
+  @IsNotEmpty()
+  @ArrayMinSize(1)
+  photo: string[];
+
+  @IsArray()
+  @IsNotEmpty()
+  type: string | PetType;
+
+  @IsString()
+  @IsOptional()
+  breed?: string;
+
+  @IsString()
+  @IsNotEmpty()
+  location: string;
+
+  @IsString()
+  @IsNotEmpty()
+  phoneNumber: string;
+}
+
+export enum PetType {
+  Dog,
+  Cat,
+}
