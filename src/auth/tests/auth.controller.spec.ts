e745a345602cfd4e5af1ee23423813984ad38f27
matheeshame <hpraveenmatheesha@gmail.com>
2022-11-18 17:13:33 +0530

    Auth test initialized

---
+++ src/auth/tests/auth.controller.spec.ts
@@ -0,0 +1,43 @@
+import { getModelToken, MongooseModule } from "@nestjs/mongoose";
+import { Test } from "@nestjs/testing";
+import { User, UserSchema } from "../../user/schema/user.schema";
+import { SchemaNames } from "../../config/SchemaNames";
+import { UserModule } from "../../user/user.module";
+import { UserService } from "../../user/user.service";
+import { AuthController } from "../auth.controller";
+import { AuthModule } from "../auth.module";
+import { AuthService } from "../auth.service";
+import { UserModel } from "./support/user.model";
+
+
+describe("Auth test", () => {
+  let authController: AuthController;
+
+  describe("Find operations", () => {
+    let userModel: UserModel;
+
+    beforeEach(async () => {
+      const moduleRef = await Test.createTestingModule({
+        controllers: [AuthController],
+        providers: [
+          UserService,
+          {
+            provide: getModelToken(SchemaNames.USER),
+            useClass: UserModel,
+          },
+          AuthService,
+        ],
+      }).compile();
+
+      authController = moduleRef.get<AuthController>(AuthController)
+      userModel = moduleRef.get<UserModel>(getModelToken(SchemaNames.USER))
+
+      jest.clearAllMocks();
+    });
+    describe("first", () => {
+      test("should first", () => {
+        console.log("test");
+      });
+    });
+  });
+});
