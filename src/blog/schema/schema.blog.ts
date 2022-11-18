fdb00057af162a9ba20971bc8d506f96dcea6ae0
matheeshame <hpraveenmatheesha@gmail.com>
2022-11-18 17:12:22 +0530

    BlogSchema

--- src/blog/schema/schema.blog.ts
+++ src/blog/schema/schema.blog.ts
@@ -1,26 +1,25 @@
-import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Types, HydratedDocument } from 'mongoose';
 import { Factory } from 'nestjs-seeder';
 
-export type BlogDocument = HydratedDocument<Blog>
+export type BlogDocument = HydratedDocument<Blog>;
 
+@Schema({ timestamps: true })
+export class Blog {
+  @Prop({ default: '636eae2e7470ba9fc521b292' })
+  owner: Types.ObjectId;
 
-@Schema({timestamps:true})
-export class Blog{
-   @Prop({default:"636eae2e7470ba9fc521b292"})
-   owner:Types.ObjectId
+  @Factory((faker) => faker.lorem.sentences(2))
+  @Prop()
+  title: string;
 
-   @Factory((faker) => faker.lorem.sentences(2))
-   @Prop()
-   title:string
+  @Factory((faker) => faker.image.animals())
+  @Prop()
+  photos: string;
 
-   @Factory((faker) => faker.image.animals())
-   @Prop()
-   photos:string
-
-   @Factory((faker) => faker.lorem.paragraph(8))
-   @Prop()
-   desc:string
+  @Factory((faker) => faker.lorem.paragraph(8))
+  @Prop()
+  desc: string;
 }
 
-export const BlogSchema = SchemaFactory.createForClass(Blog)
+export const BlogSchema = SchemaFactory.createForClass(Blog);
