fc2548f1c08215c5610505d896264056a855c99e
matheeshame <hpraveenmatheesha@gmail.com>
2022-11-18 17:11:04 +0530

    UserDec

--- src/shop/shop.controller.ts
+++ src/shop/shop.controller.ts
@@ -4,7 +4,7 @@ import { Body, Param, Put, UseGuards } from '@nestjs/common/decorators';
 import { ShopService } from './shop.service';
 import { AuthGuard } from '@nestjs/passport';
 import { AdminGuard } from 'src/guards/admin.guard';
-import { UserDec } from 'src/utilities/user.decorator';
+import { UserDec } from '../utilities/user.decorator';
 import { User } from '../user/schema/user.schema';
 import { UpdateShopDTO } from './dto/update-shop.dto';
 import { Request } from 'express';
