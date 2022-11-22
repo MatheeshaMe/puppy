6cdd126b9e0c8aba5d05328dace19df4083df534
matheeshame <hpraveenmatheesha@gmail.com>
2022-11-22 16:08:35 +0530

    rm tests

--- src/auth/__mocks__/users.service.ts
+++
@@ -1,8 +0,0 @@
-import { userStub } from "../tests/stubs/user.stub";
-
-export const UsersService = jest.fn().mockReturnValue({
-   register:jest.fn().mockResolvedValue(userStub()),
-   login:jest.fn().mockResolvedValue(userStub()),
-   findByPayload:jest.fn().mockResolvedValue(userStub()),
-   updateUser:jest.fn().mockResolvedValue(userStub()),
-})
--- src/auth/tests/support/user.model.ts
+++
@@ -1,7 +0,0 @@
-import { MockModel } from "../../../auth/__mocks__/mock.model";
-import { User } from "../../../user/schema/user.schema";
-import { userStub } from "../stubs/user.stub";
-
-export class UserModel extends MockModel<User>{
-    protected ustub = userStub();
-}
