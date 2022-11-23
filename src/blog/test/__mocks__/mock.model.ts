59086fab93e6a30c9f458849e3a4f113d88f081e
matheeshame <hpraveenmatheesha@gmail.com>
2022-11-23 15:16:01 +0530

    MockBlogModel

---
+++ src/blog/test/__mocks__/mock.model.ts
@@ -0,0 +1,30 @@
+import { Blog } from "src/blog/schema/schema.blog"
+import { User } from "src/user/schema/user.schema"
+
+export abstract class MockBlogModel<T>{
+    protected abstract blogStub:T
+
+    constructor(createBlogData:T){
+        this.constructorSpy(createBlogData)
+    }
+
+    constructorSpy(_createBlogData:T):void{}
+
+    async findById():Promise<T>{
+     return this.blogStub
+    }
+
+    async find():Promise<T[]>{
+        return [this.blogStub]
+    }
+    
+    async findByIdAndUpdate():Promise<T>{
+        return this.blogStub
+    }
+    async save():Promise<T>{
+        return this.blogStub
+    }
+    // async findByIdAndDelete():Promise<{message:string}>{
+    //     return {message}
+    // }
+}
