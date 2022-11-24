dadfde09cfe8fcc64b6abfc74d71c0790b0d67a0
matheeshame <hpraveenmatheesha@gmail.com>
2022-11-24 19:17:42 +0530

    MockBlogModel

---
+++ src/blog/test/__mocks__/mock.model.ts
@@ -0,0 +1,33 @@
+
+
+export abstract class MockBlogModel<T>{
+    protected abstract blogStub:T
+
+    constructor(createBlogData:T){
+        this.constructorSpy(createBlogData)
+    }
+
+    constructorSpy(_createBlogData:T):void{}
+
+    async findById():Promise<T>{
+     return this.blogStub
+    }
+
+    async find():Promise<T[]>{
+        return [this.blogStub]
+    }
+    
+    async findByIdAndUpdate():Promise<T>{
+        return this.blogStub
+    }
+    async save():Promise<T>{
+        return this.blogStub
+    }
+
+    async findByIdAndDelete():Promise<{}>{
+        return {}
+    }
+    // async findByIdAndDelete():Promise<{message:string}>{
+    //     return {message}
+    // }
+}
